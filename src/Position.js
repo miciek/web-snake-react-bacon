5ba5f293f3f21ea17f2c9fdd05a43c05f0f92524
miciek <miciek@gmail.com>
2015-09-18 22:26:20 +0200

    Position transformed into ES6

--- src/Position.js
+++ src/Position.js
@@ -1,30 +1,33 @@
-function Position(x, y) {
-    this.x = x;
-    this.y = y;
-    this.equals = function (p) {
+export default class Position {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    equals(p) {
         return this.x === p.x && this.y === p.y;
-    };
-    this.add = function (p) {
+    }
+
+    add(p) {
         return new Position(this.x + p.x, this.y + p.y);
-    };
-    this.mod = function (size) {
+    }
+
+    mod(size) {
         return new Position((this.x + size.x) % size.x, (this.y + size.y) % size.y);
-    };
+    }
+
+    static randomPosition(size) {
+        return new Position(
+            Math.floor(Math.random() * size.x),
+            Math.floor(Math.random() * size.y)
+        );
+    }
+
+    static rotateRight(pos) {
+        return new Position(-pos.y, pos.x);
+    }
+
+    static rotateLeft(pos) {
+        return new Position(pos.y, -pos.x);
+    }
 }
-
-Position.randomPosition = function(size) {
-    return new Position(
-        Math.floor(Math.random() * size.x),
-        Math.floor(Math.random() * size.y)
-    );
-};
-
-Position.rotateRight = function(pos) {
-    return new Position(-pos.y, pos.x);
-};
-
-Position.rotateLeft = function(pos) {
-    return new Position(pos.y, -pos.x);
-};
-
-export default Position;
