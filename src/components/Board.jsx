465de55855081f71fc617fdfb381f4fe6036a2e9
miciek <miciek@gmail.com>
2015-09-19 23:46:18 +0200

    converted Board component into ES6

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,25 +6,25 @@ import tools from "../tools"
 
 import styles from "../style.css"
 
-export default React.createClass({
-    propTypes: {
+export default class Board extends React.Component {
+    static propTypes = {
         snakePositions: React.PropTypes.arrayOf(Position).isRequired,
         fruitPositions: React.PropTypes.arrayOf(Position).isRequired,
-        size: React.PropTypes.instanceOf(Position).isRequired
-    },
+        size: React.PropTypes.instanceOf(Position).isRequired,
+    }
 
-    render: function () {
-        var rows = _.range(this.props.size.y).map(function (y) {
-            var cells = _.range(this.props.size.x).map(function (x) {
-                var className = styles.cell
+    render = () => {
+        const rows = _.range(this.props.size.y).map(y => {
+            const cells = _.range(this.props.size.x).map(x => {
+                let className = styles.cell
                 if (tools.contains(this.props.snakePositions, new Position(x, y))) className += " " + styles.snake
                 if (tools.contains(this.props.fruitPositions, new Position(x, y))) className += " " + styles.fruit
 
                 return <div key={"r" + y + "c" + x} className={className}/>
-            }.bind(this))
+            })
             return <div key={y} className={styles.row}>{cells}</div>
-        }.bind(this))
+        })
 
         return <div className={styles.board}>{rows}</div>
     }
-})
+}
