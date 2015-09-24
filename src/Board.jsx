c68d868fec352b004bac9b857786568375082736
miciek <miciek@gmail.com>
2015-09-24 08:22:15 +0200

    switched to classNames lib for conditional class names

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,6 +5,7 @@ import Vector from "./Vector"
 import tools from "./tools"
 
 import styles from "./style.css"
+import classNames from "classnames"
 
 export default class Board extends React.Component {
     static propTypes = {
@@ -13,14 +14,21 @@ export default class Board extends React.Component {
         size: React.PropTypes.instanceOf(Vector).isRequired,
     }
 
+    isSnakePosition(position) {
+      return tools.contains(this.props.snakePositions, position)
+    }
+
+    isFruitPosition(position) {
+      return tools.contains(this.props.fruitPositions, position)
+    }
+
     render() {
         const rows = _.range(this.props.size.y).map(y => {
             const cells = _.range(this.props.size.x).map(x => {
-                let className = styles.cell
-                if (tools.contains(this.props.snakePositions, new Vector(x, y))) className += " " + styles.snake
-                if (tools.contains(this.props.fruitPositions, new Vector(x, y))) className += " " + styles.fruit
-
-                return <div key={"r" + y + "c" + x} className={className}/>
+              const pos = new Vector(x, y)
+              const snakeStyle = { [styles.snake]: this.isSnakePosition(pos) }
+              const fruitStyle = { [styles.fruit]: this.isFruitPosition(pos) }
+              return <div key={x} className={ classNames(styles.cell, snakeStyle, fruitStyle)}/>
             })
             return <div key={y} className={styles.row}>{cells}</div>
         })
