c47fd498d11e6cea6b8283f81f43ec8b7dbb3ba9
miciek <miciek@gmail.com>
2016-01-09 22:17:22 +0100

    added WebSocket and basic support for game events

---
+++ src/MultiSnakeGame.jsx
@@ -0,0 +1,71 @@
+import React from "react"
+import Bacon from "baconjs"
+import _ from "underscore"
+
+import Vector from "./Vector"
+import Board from "./Board"
+import style from "./style"
+
+export default class MultiSnakeGame extends React.Component {
+  static propTypes = {
+    boardSize: React.PropTypes.instanceOf(Vector).isRequired,
+    gameEvents: React.PropTypes.instanceOf(Bacon.EventStream).isRequired,
+    onNewSnake: React.PropTypes.func.isRequired
+  }
+
+  static defaultProps = {
+    initialSnakePosition: new Vector(0, 0),
+    initialSnakeDirection: new Vector(0, 1),
+    initialSnakeLength: 3
+  }
+
+  state = {
+    snakePositions: [],
+    fruitPosition: Vector.random(this.props.boardSize),
+    score: 0
+  }
+
+  inputStreams() {
+    const ticks = Bacon.interval(100)
+    const keys = Bacon.fromEvent(document.body, "keyup").map(".keyCode")
+    const lefts = keys.filter(key => key === 37)
+    const rights = keys.filter(key => key === 39)
+    return { ticks: ticks, lefts: lefts, rights: rights }
+  }
+
+  snakeHeadPositions({ ticks, lefts, rights }) {
+    const leftRotations = lefts.map(() => Vector.rotateLeft)
+    const rightRotations = rights.map(() => Vector.rotateRight)
+    const actions = leftRotations.merge(rightRotations)
+
+    const directions = actions.scan(this.props.initialSnakeDirection, (dir, f) => f(dir))
+    return directions
+              .sampledBy(ticks)
+              .scan(this.props.initialSnakePosition, (pos, dir) => pos.add(dir).mod(this.props.boardSize))
+  }
+
+  componentDidMount() {
+    const snakeHeadPositions = this.snakeHeadPositions(this.inputStreams())
+    const snakes = snakeHeadPositions.scan([], (snake, head) => {
+      const biggerSnake = _.union(snake, [head])
+      const validSnake = _.last(biggerSnake, this.props.initialSnakeLength + this.state.score)
+      return validSnake
+    })
+    snakes.onValue(snake => this.setState({ snakePositions: snake }))
+    snakes.onValue(snake => this.props.onNewSnake(snake))
+
+    const fruitEatenEvents = snakeHeadPositions.filter(head => head.equals(this.state.fruitPosition))
+    fruitEatenEvents.onValue(() => this.setState({ score: this.state.score + 1 }))
+    fruitEatenEvents.map(() => Vector.random(this.props.boardSize))
+                    .onValue(fruit => this.setState({ fruitPosition: fruit }))
+  }
+
+  render() {
+    return (
+      <div className={style.game}>
+        <div className={style.log}>Score: {this.state.score}</div>
+        <Board size={this.props.boardSize} snakePositions={this.state.snakePositions} fruitPosition={this.state.fruitPosition}/>
+      </div>
+    )
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import React from "react"
 import "./style.css"
 import Vector from "./Vector"
 import SnakeGame from "./SnakeGame"
+import MultiSnakeGame from "./MultiSnakeGame"
 import Board from "./Board"
+import Bacon from "baconjs"
 
-React.render(<SnakeGame boardSize={new Vector(20, 20)} />, document.getElementById("app"))
+// TODO: what happens when server is unavailable for a while and socket closes?
+const ws = new WebSocket("ws://localhost:8080/snakeSocket")
+const playerName = window.location.pathname.substr(1)
+
+ws.onopen = () => {
+  const gameEvents = Bacon.fromEvent(ws, "message").map(".data")
+
+  const newSnakeCallback = (snakePositions) => {
+    ws.send(JSON.stringify({ name: playerName, s: snakePositions }))
+  }
+
+  React.render(<MultiSnakeGame boardSize={new Vector(20, 20)} gameEvents={gameEvents} onNewSnake={newSnakeCallback} />, document.getElementById("app"))
+}
+
+// TODO: single or multiplayer mode switched by URL param
+ws.onerror = () => {
+  React.render(<SnakeGame boardSize={new Vector(20, 20)} />, document.getElementById("app"))
+}
