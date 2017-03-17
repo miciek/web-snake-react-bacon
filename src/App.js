422b946542977d14305fea5d6549633a719af86e
miciek <miciek@gmail.com>
2017-03-17 23:24:48 +0100

    Remove SnakeGame and Board implementations and add workshop agenda

--- src/App.js
+++ src/App.js
@@ -4,6 +4,50 @@ import SnakeGame from "./SnakeGame";
 
 export default class App extends Component {
   render() {
-    return <SnakeGame boardSize={new Vector(20, 20)} />;
+    return <div>TODO</div>;
   }
 }
+
+// 0. Setup
+// - create-react-app (npm install -g create-react-app)
+// - Vector
+// - index.html
+
+// 1. Static board
+// - propTypes: component API, checked by React
+// - styles: Board.css
+// - React warnings: class vs className, lack of keys
+// - classNames function
+
+// 2. SnakeGame
+// - composition
+// - state: component is still a function
+// - random fruit in state
+
+// 3. Snake's moving
+// - component lifecycle (componentDidMount)
+// - ticks stream (onValue, log)
+// - initial position & direction (defaultProps)
+// - directions stream v1 (map marble)
+// - heads stream (scan marble)
+// - setState
+
+// 4. Snake's changing direction
+// - (refactor componentDidMount into inputStreams and snakeHeadPositions)
+// - keys stream (filter marble)
+// - actions stream (merge marble)
+// - directions stream - snake's head is moving on key press (scan marble again)
+// - snake's head is moving (sampledBy marble)
+
+// 5. Snake is 3 cells long
+// - slidingWindow
+
+// 6. Snake eats fruits
+// - fruitEatenEvents stream
+// - score on fruitEatenEvent (styles: SnakeGame.css)
+// - new random fruit on fruitEatenEvent
+// - slidingWindow doesn't work?
+// - snake is getting bigger (scan marble again)
+
+// 7. Game restarts when Snake hits itself
+// - gameOverEvents stream
--- src/Board.jsx
+++ src/Board.jsx
@@ -3,28 +3,3 @@ import _ from "underscore";
 import classNames from "classnames";
 import Vector from "./Vector";
 import "./Board.css";
-
-export default class Board extends Component {
-  static propTypes = {
-    size: PropTypes.instanceOf(Vector).isRequired,
-    snakePositions: PropTypes.arrayOf(PropTypes.instanceOf(Vector)).isRequired,
-    fruitPosition: PropTypes.instanceOf(Vector).isRequired
-  };
-
-  render() {
-    const { size, snakePositions, fruitPosition } = this.props;
-    const rows = _.range(size.y).map(y => {
-      const cells = _.range(size.x).map(x => {
-        const pos = new Vector(x, y);
-        const maybeSnakeStyle = {
-          snake: snakePositions.find(s => s.equals(pos))
-        };
-        const maybeFruitStyle = { fruit: fruitPosition.equals(pos) };
-        return <div key={x} className={classNames("cell", maybeSnakeStyle, maybeFruitStyle)} />;
-      });
-      return <div key={y} className="row">{cells}</div>;
-    });
-
-    return <div className="board">{rows}</div>;
-  }
-}
